77d8c69b337733715d62c21bae51453a007678a5
Gouravpreet Singh Sandhu <gauravpreet2005@gmail.com>
2026-09-22 07:23:55 +0530

    Added soundtrack and fixed aspect Ratio problems

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,10 +4,21 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
     [SerializeField] private AudioSource sfxSource;
+    public AudioClip clip;
 
     void Awake()
     {
+         if (instance == null)
+    {
         instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    else
+    {
+        Destroy(gameObject);
+    }
+
+    PlaySFX(clip);
     }
 
     public void PlaySFX(AudioClip clip)
